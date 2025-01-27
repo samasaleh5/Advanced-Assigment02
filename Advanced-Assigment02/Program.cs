f68bf393a19093aa9423218a624436a1939f2d75
samasaleh5 <ss2104@fayoum.edu.eg>
2025-01-27 20:32:13 +0200

    Question2 solved

--- Advanced-Assigment02/Program.cs
+++ Advanced-Assigment02/Program.cs
@@ -1,44 +1,75 @@
-﻿using System.Collections;
-
-namespace Advanced_Assigment02
+﻿namespace Advanced_Assigment02
 {
-    internal class Program
-    {
+    internal class Program {
+        #region PalindromeFunction
+        static bool isPalindrome(List<int> list)
+        {
+            int left = 0; int right = list.Count - 1;
+            while (left < right)
+            {
+                if (list[left] != list[right])
+                    return false;
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        #endregion
         static void Main(string[] args)
         {
             #region Question1
-            Console.WriteLine("Enter size of array Then Enter Number of Queries");
+            //Console.WriteLine("Enter size of array Then Enter Number of Queries");
+            //int ArraySize = int.Parse(Console.ReadLine());
+            //List<int> arrayList = new List<int>(ArraySize);
+            //int Qsize = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Array Elements");
+            //for (int i = 0; i < ArraySize; i++)
+            //{
+            //    arrayList.Add(int.Parse(Console.ReadLine()));
+            //}
+            //Console.WriteLine("Enter Queries");
+            //List<int> resultcounter = new List<int>();
+            //for (int i = 0; i < Qsize; i++)
+            //{
+            //    int X = int.Parse(Console.ReadLine());
+            //    int count = 0;
+            //    for (int j = 0; j < ArraySize; j++)
+            //    {
+            //        if (arrayList[j] > X)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    resultcounter.Add(count);
+            //}
+            //Console.WriteLine("OutPut : ");
+            //foreach (int i in resultcounter)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+            #region Question2
+            Console.WriteLine("Input: ");
             int ArraySize = int.Parse(Console.ReadLine());
             List<int> arrayList = new List<int>(ArraySize);
-            int Qsize = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("Enter List Elements : ");
             for (int i = 0; i < ArraySize; i++)
             {
                 arrayList.Add(int.Parse(Console.ReadLine()));
             }
-            Console.WriteLine("Enter Queries");
-            List<int> resultcounter = new List<int>();
-            for (int i = 0; i < Qsize; i++)
+            Console.WriteLine("Output:");
+            if (isPalindrome(arrayList))
             {
-                int X = int.Parse(Console.ReadLine());
-                int count = 0;
-                for (int j = 0; j < ArraySize; j++)
-                {
-                    if (arrayList[j] > X)
-                    {
-                        count++;
-                    }
-                }
-                resultcounter.Add(count);
+                Console.WriteLine("YES");
             }
-            Console.WriteLine("OutPut : ");
-            foreach (int i in resultcounter)
-            {
-                Console.WriteLine(i);
-            }
+            else
+                Console.WriteLine("Not Palindrome");
+
             #endregion
+            #region Question3
 
-
+            #endregion
         }
-    }
+        }
 }
